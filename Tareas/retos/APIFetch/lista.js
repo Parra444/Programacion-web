8a0edf7bca849b349324a93eb0aae0921d0625fb
IUE <ssss>
2022-05-21 12:07:17 -0500

    apiysopalistas

--- Tareas/retos/APIFetch/lista.js
+++ Tareas/retos/APIFetch/lista.js
@@ -1,76 +1,40 @@
-// const cargar = (document.querySelector("body").onload = function() {
-//     const p1 = leerestudiantes();
-// });
 
 const load = document.querySelector("#contenido1");
 
-// function leerlista(url){
-//     const url = "/Tareas/retos/APIFetch/estudiantes.json"
-//     const lista = fetch(url)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data);
-//             return data})
-// }
-
-// function leerestudiantes(){
-//     //var contenido1=document.querySelector("#contenido1");
-//     fetch("/Tareas/retos/APIFetch/estudiantes1.json")
-//         .then(res => res.json())
-//         .then(data => {
-//             data.forEach(dato => {
-//                 console.log(dato)
-//                 const col = document.createElement("tr");
-//                 col.innerHTML+=`<td>${dato.estudiantes.alumno}</td>`;
-//                 load.appendChild(col)
-//             });
-            
-
-            
-
-
-//             // var cedula =
-//             //     `<p>Cedula: ${data.estudiantes["0"].alumno.cedula}</p>`;
-//             // var nombre =
-//             //     `<h2 class='name'>Nombre: ${data.estudiantes["1"].alumno.nombre}</h2>`;
-//             // var asignatura =
-//             //     `<p>Asignatura: ${data.estudiantes["1"].alumno.asignaturas["2"]}</p>`;
-//             // var programa =
-//             // `<p>Programa: ${data.estudiantes["1"].alumno.programa}</p>`;
-            
-//             // var foto =
-//             //     `<img src= ${data.estudiantes["1"].alumno.foto}alt='Foto' width='250px' class ='img-fluid rounded-circle'>`;
-        
-//             //contenido1.innerHTML = cedula + nombre + asignatura + programa + foto;
-//         });
-// }
-// leerestudiantes();
-
 function carga1(){
+    let i=0;
+    var content="";
     console.log(fetch("estudiantes.json"));
     fetch("estudiantes.json")
         .then(res => res.json())
-        // .then(estudiantes => {
-        //     estudiantes.forEach(lista => {
-        //         console.log(lista)
-        //         const dato = document.createElement("tr");
-        //         dato.innerHTML += `<td> ${lista.estudiantes.}</td>` ;
-        //         load.appendChild(dato)
-        //     });
-        // })
         .then(data => {
-            console.log(data)
+            let listaestudiantes = Object.values(data.estudiantes);
+            //console.log(listaestudiantes.length)
+            var titulo = `<h1>ESTUDIANTES</h1>`;
+            var row1= `<div class = "fila">`;
+            var row2= `</div>`;
+            var btn= `<div class="filaboton">
+            <div class="boton">
+              <a href="/Tareas/index.html">Volver al inicio</a>
+            </div>
+          </div>`
             
-            var cedula =`<p>Cedula: ${data.estudiantes["1"].alumno.cedula}</p>`;
-            var nombre =`<h2 class='name'>Nombre: ${data.estudiantes["1"].alumno.nombre}</h2>`;
-            var asignatura =`<p>Asignatura: ${data.estudiantes["1"].alumno.asignaturas["2"]}</p>`;
-            var programa =`<p>Programa: ${data.estudiantes["1"].alumno.programa}</p>`;
-            var foto =`<img src= ${data.estudiantes["1"].alumno.foto}alt='Foto' width='250px' class ='img-fluid rounded-circle'>`;
-        
-            contenido1.innerHTML = cedula + nombre + asignatura + programa + foto;
+            for(i ; i< listaestudiantes.length; i++)
+            {
+                
+                var div1 = `<div class = "ficha">`
+                var cedula =`<p>Cedula: ${data.estudiantes[i].alumno.cedula}</p>`;
+                var nombre =`<h2 class='name'>${data.estudiantes[i].alumno.nombre}</h2>`;
+                var asignatura =`<p>Asignaturas: ${data.estudiantes[i].alumno.asignaturas}</p>`;
+                var programa =`<p>Programa: ${data.estudiantes[i].alumno.programa}</p>`;
+                var foto =`<img src= ${data.estudiantes[i].alumno.foto} alt='Foto' width='250px' class ='img-fluid rounded-circle'>`;
+                var div2 = `</div>`
+                content += div1 +nombre + cedula + asignatura + programa + foto + div2;
+            }
+            
+            load.innerHTML = titulo + row1 + content+ row2 + btn;
         });
 
 }
-
 
 carga1();
